388b44edf52d6cb45410f8d478800528cf16c735
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-08-05 20:08:02 +0530

    fix the rate limiting issue

--- 02.thinkboard/frontend/src/pages/HomePage.jsx
+++ 02.thinkboard/frontend/src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ import NoteCard from "../components/NoteCard.jsx";
 import NotesNotFound from "../components/NotesNotFound.jsx";
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -41,13 +41,13 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUi />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && (
+        {loading && !isRateLimited && (
           <div className="text-center text-primary py-10">Loading notes...</div>
         )}
 
-        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+        {!loading && notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
-        {notes.length > 0 && !isRateLimited && (
+        {!loading && notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
               <div>
