0c2d6d742b31a8244ad58daf0d8ebfccbc61fe53
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-08-05 18:14:33 +0530

    update axios file

--- 02.thinkboard/frontend/src/lib/axios.js
+++ 02.thinkboard/frontend/src/lib/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.NODE === "development" ? "http://localhost:5001/api" : "/api";
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:5001/api"
+    : "https://thinkboard-backend-tb9x.onrender.com/api",
 });
 
 export default api;
