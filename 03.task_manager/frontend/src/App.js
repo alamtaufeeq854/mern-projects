24c00d7a58a52b2ddbc39a80c5d666aa0e3db0f2
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-08-13 08:49:03 +0530

    add back button

--- 03.task_manager/frontend/src/App.js
+++ 03.task_manager/frontend/src/App.js
@@ -3,6 +3,11 @@ import TaskManager from "./TaskManager";
 function App() {
   return (
     <div className="App">
+      <a
+        href="https://alamtaufeeq854.github.io/mern-projects/"
+        className="btn btn-primary m-3">
+        Back
+      </a>
       <TaskManager />
     </div>
   );
