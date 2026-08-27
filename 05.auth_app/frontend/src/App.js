67f07f7d6b396b4a63e56af96f75e57edbf5386d
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-08-27 20:20:04 +0530

    add back button

--- 05.auth_app/frontend/src/App.js
+++ 05.auth_app/frontend/src/App.js
@@ -14,6 +14,15 @@ const App = () => {
 
   return (
     <div className="App">
+      <button
+        className="back"
+        onClick={() =>
+          (window.location.href =
+            "https://alamtaufeeq854.github.io/mern-projects/")
+        }>
+        Back
+      </button>
+
       <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
